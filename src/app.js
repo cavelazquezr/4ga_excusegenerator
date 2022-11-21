6790412f0593cee9e7e3b4aff28de4ed9ad88bfd
Carlos Alberto <95.carlos.velazquez@gmail.com>
2022-11-21 10:44:54 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,47 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird", "That rat"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+let emoji = ["😩", "😭", "😖", "😓", "🥵", "😰", "🤯"];
+
+let pExcuse = document.querySelector("#excuse");
+let excuseButton = document.querySelector("#BtnGenerateExcuse");
+
+const excuseGenerator = () => {
+  let excuse = "";
+
+  //for who
+  excuse += who[Math.floor(Math.random() * who.length)];
+
+  //for action
+  excuse += " " + action[Math.floor(Math.random() * action.length)];
+
+  //for what
+  excuse += " " + what[Math.floor(Math.random() * what.length)];
+
+  //for when
+  excuse += " " + when[Math.floor(Math.random() * when.length)];
+
+  //for emoji
+  excuse += " " + emoji[Math.floor(Math.random() * emoji.length)] + ".";
+
+  return excuse;
+};
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  pExcuse.innerHTML = excuseGenerator();
 };
+
+excuseButton.addEventListener("click", function() {
+  pExcuse.innerHTML = excuseGenerator();
+});
